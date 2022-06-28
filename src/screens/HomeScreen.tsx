c637e6779144c5bc07a8451f821fe1652e5858b5
= <=>
2022-06-27 20:35:21 -0500

    navigation with props

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import { useNavigation } from '@react-navigation/native'
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
 import { Button, Text, View } from 'react-native'
 
-export const HomeScreen = () => {
-  const navigation =  useNavigation();
+
+interface Props extends StackScreenProps<any,any> {}
+
+export const HomeScreen = ({ navigation}:Props) => {
+
   return (
     <View>
         <Text>HomeScreen</Text>
