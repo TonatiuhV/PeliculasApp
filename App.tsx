c648f75d0bc29f3b1fbb7025c863e1c2df433296
= <=>
2022-06-27 20:14:06 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native'
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>
+          Hola mundo
+        </Text>
+      </View>
+    </NavigationContainer>
+  )
+}
+
+export default App;
