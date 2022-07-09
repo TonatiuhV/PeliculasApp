ebb0827da12e7e68f424e561e1fbb4b8406d20f7
= <=>
2022-07-08 22:26:35 -0500

    Gradient Context

--- App.tsx
+++ App.tsx
@@ -4,13 +4,25 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
 import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+
+const AppState = ({children}:any) => {
+  return (
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  )
+}
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <FadeScreen/> */}
-      <Navigation/>
+      <AppState>
+        {/* <FadeScreen/> */}
+        <Navigation/>
+      </AppState>
     </NavigationContainer>
   )
 }
---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,51 @@
+
+import React, { createContext, useState } from "react";
+
+
+interface ImageColors {
+    primary:string,
+    secoundary:string,
+}
+
+interface GradientContextProps {
+    colors: ImageColors;
+    prevColors: ImageColors;
+    setMainColors: (colors: ImageColors) => void; 
+    setPrevMainColors: (colors: ImageColors) => void;
+}
+
+export const GradientContext = createContext({}as GradientContextProps) // TODO: definer un tipo
+
+
+export const GradientProvider = ({children}:any) => {
+    const [colors, setColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secoundary: 'transparent'
+    });
+
+    const [prevColors, setPrevColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secoundary: 'transparent'
+    })
+
+    const setMainColors = (colors:ImageColors) => {
+        setColors(colors)
+    }
+    const setPrevMainColors = (colors:ImageColors) => {
+        setPrevColors(colors)
+    } 
+    
+    
+    return (
+        <GradientContext.Provider
+            value={{
+                colors,
+                prevColors,
+                setMainColors,
+                setPrevMainColors
+            }}
+        >
+            {children}
+        </GradientContext.Provider>
+    )
+}
